d91a7b37de0777bc2c1607d2386d0778047aa299
YoginiK2023 <101918639+YoginiK2023@users.noreply.github.com>
2022-03-19 12:05:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -18,7 +18,9 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='reported as frauds : {}'.format(output))
+
+     return render_template('index.html', prediction_text='Frauds Trascations : {}'.format(output))
 
 @app.route('/results',methods=['POST'])
 def results():
